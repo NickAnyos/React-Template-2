67b6763f119713c1dfe72e3af71d360e8b5889ea
Nick Anyos <nick.anyos@gmail.com>
2024-07-01 09:53:28 +1000

    Changed vite template to remove boilerplate

---
+++ src/App/App.tsx
@@ -0,0 +1,11 @@
+import React from "react"
+import "./App.css"
+interface AppProps {
+	// Props
+}
+
+const App: React.FC<AppProps> = props => {
+	return <div className="App">App</div>
+}
+
+export default App
